69f0073474c498fdaa8b8e0bcc4440c94e9bd978
Leo Yan <leo.yan@linaro.org>
2017-07-04 09:00:53 +0800

    Fixup #2
    Signed-off-by: Leo Yan <leo.yan@linaro.org>

--- result_comparision.py
+++ result_comparision.py
@@ -11,100 +11,103 @@ import re
 from os import system, remove
 from collections import defaultdict
 
-scenarios = {
+class result_comparison:
 
-    'idle' : [
-        'Power',
-    ],
+    scenarios = {
+        'idle' : [
+            'Power',
+        ],
+        'audio' : [
+            'Power',
+        ],
+        'video' : [
+            'Power',
+        ],
+        'hackbench' : [
+            'Power',
+        ],
+        'geekbench' : [
+            'score',
+            'multicore_score',
+            'Power',
+        ],
+    }
 
-    'audio' : [
-        'Power',
-    ],
-
-    'video' : [
-        'Power',
-    ],
-
-    'hackbench' : [
-        'Power',
-    ],
-
-    'geekbench' : [
-        'score',
-        'multicore_score',
-        'Power',
-    ],
-}
-
-#def tree():
-#    return collections.defaultdict(tree)
-
-
-def parse_file(scene, file_name):
-
-    #print file_name
-    #print file_name.split(os.path.sep)[0]
-
-    in_file = open(file_name, "r")
-    in_line = csv.reader(in_file)
-    next(in_line, None)
-
-    l1_Value = dict()
-    #l2_Value = defaultdict(list)
     sections = []
 
-    p = re.compile(r'(.*)_\d', re.I)
+    def __init__(self, infile, outfile):
+        self.infile = infile
 
-    for row in in_line:
-        m = re.match(p, row[0])
+    def __del__(self):
+        self.fr.close()
 
-        if row[1] not in scene:
-            continue
+    def parse_file(self):
+        for s in self.scenarios:
+            self.parse_scene(s)
 
-        section = m.group(1)
-        #print section
+    def parse_sections(self):
 
-        if not section in sections:
-            sections.append(section)
+        sec = []
 
-        #print sections
-
-    for kern in sections:
-
-        l2_Value = defaultdict(list)
-
-        in_file = open(file_name, "r")
-        in_line = csv.reader(in_file)
+        self.fr = open(self.infile, "r")
+        in_line = csv.reader(self.fr)
         next(in_line, None)
 
+        p = re.compile(r'(.*)_\d', re.I)
+
         for row in in_line:
-            #print row[0]
-            #print row[1]
-            #print row[3]
-            #print row[4]
-
             m = re.match(p, row[0])
-            #print m.group(1)
+            section = m.group(1)
 
-            if row[1] not in scene:
-                continue
+            if not section in sec:
+                sec.append(section)
 
-            section    = m.group(1)
-            condition = row[3]
-            value     = row[4]
+        return sec
 
-            if condition not in scenarios[scene]:
-                continue
+    def parse_scene(self, scene):
 
-            #print section
-            #print kern
-            if section != kern:
-                continue
+        l1_Value = dict()
 
-            l2_Value[condition].append(float(value))
-            l1_Value[section] = l2_Value
+        self.sections = self.parse_sections()
 
-    print l1_Value
+        p = re.compile(r'(.*)_\d', re.I)
+
+        for kern in self.sections:
+
+            l2_Value = defaultdict(list)
+
+            self.fr = open(self.infile, "r")
+            in_line = csv.reader(self.fr)
+            next(in_line, None)
+
+            for row in in_line:
+                #print row[0]
+                #print row[1]
+                #print row[3]
+                #print row[4]
+
+                m = re.match(p, row[0])
+                #print m.group(1)
+
+                if row[1] not in scene:
+                    continue
+
+                section    = m.group(1)
+                condition = row[3]
+                value     = row[4]
+
+                if condition not in self.scenarios[scene]:
+                    continue
+
+                #print section
+                #print kern
+                if section != kern:
+                    continue
+
+                l2_Value[condition].append(float(value))
+                l1_Value[section] = l2_Value
+
+        print l1_Value
 
 
 def main(argv):
@@ -113,8 +116,10 @@ def main(argv):
         print "file don't exists:"
         sys.exit(1)
 
-    for s in scenarios:
-        parse_file(s, sys.argv[1])
+    #for s in scenarios:
+    #    parse_file(s, sys.argv[1])
+    parser = result_comparison(sys.argv[1], sys.argv[2])
+    parser.parse_file()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
